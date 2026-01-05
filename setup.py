883c37bb5f9861d8fc54584e36c90e8133b991d7
Tanujkumar24 <tanuj.mangalapally@gmail.com>
2026-01-05 23:34:29 +0530

    first commit installed all dependencies

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Tanujkumar",
+    author_email="tanuj.mangalapally@gmail.com",
+    packages=find_packages()
+)

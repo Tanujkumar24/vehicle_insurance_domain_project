ef894b3ba51f5665d8bdf89cf4885c0fe6a06661
Tanujkumar24 <tanuj.mangalapally@gmail.com>
2026-01-07 20:37:05 +0530

    training pipeline is created

--- src/pipline/training_pipeline.py
+++ src/pipline/training_pipeline.py
@@ -1,110 +1,146 @@
 import sys
-from src.entity.config_entity import VehiclePredictorConfig
-from src.entity.s3_estimator import Proj1Estimator
 from src.exception import MyException
 from src.logger import logging
-from pandas import DataFrame
+
+from src.components.data_ingestion import DataIngestion
+from src.components.data_validation import DataValidation
+from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
+from src.components.model_evaluation import ModelEvaluation
+from src.components.model_pusher import ModelPusher
+
+from src.entity.config_entity import (DataIngestionConfig,
+                                          DataValidationConfig,
+                                          DataTransformationConfig,
+                                          ModelTrainerConfig,
+                                          ModelEvaluationConfig,
+                                          ModelPusherConfig)
+                                          
+from src.entity.artifact_entity import (DataIngestionArtifact,
+                                            DataValidationArtifact,
+                                            DataTransformationArtifact,
+                                            ModelTrainerArtifact,
+                                            ModelEvaluationArtifact,
+                                            ModelPusherArtifact)
 
 
-class VehicleData:
-    def __init__(self,
-                Gender,
-                Age,
-                Driving_License,
-                Region_Code,
-                Previously_Insured,
-                Annual_Premium,
-                Policy_Sales_Channel,
-                Vintage,
-                Vehicle_Age_lt_1_Year,
-                Vehicle_Age_gt_2_Years,
-                Vehicle_Damage_Yes
-                ):
+
+class TrainPipeline:
+    def __init__(self):
+        self.data_ingestion_config = DataIngestionConfig()
+        self.data_validation_config = DataValidationConfig()
+        self.data_transformation_config = DataTransformationConfig()
+        self.model_trainer_config = ModelTrainerConfig()
+        self.model_evaluation_config = ModelEvaluationConfig()
+        self.model_pusher_config = ModelPusherConfig()
+
+
+    
+    def start_data_ingestion(self) -> DataIngestionArtifact:
         """
-        Vehicle Data constructor
-        Input: all features of the trained model for prediction
+        This method of TrainPipeline class is responsible for starting data ingestion component
         """
         try:
-            self.Gender = Gender
-            self.Age = Age
-            self.Driving_License = Driving_License
-            self.Region_Code = Region_Code
-            self.Previously_Insured = Previously_Insured
-            self.Annual_Premium = Annual_Premium
-            self.Policy_Sales_Channel = Policy_Sales_Channel
-            self.Vintage = Vintage
-            self.Vehicle_Age_lt_1_Year = Vehicle_Age_lt_1_Year
-            self.Vehicle_Age_gt_2_Years = Vehicle_Age_gt_2_Years
-            self.Vehicle_Damage_Yes = Vehicle_Damage_Yes
-
+            logging.info("Entered the start_data_ingestion method of TrainPipeline class")
+            logging.info("Getting the data from mongodb")
+            data_ingestion = DataIngestion(data_ingestion_config=self.data_ingestion_config)
+            data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+            logging.info("Got the train_set and test_set from mongodb")
+            logging.info("Exited the start_data_ingestion method of TrainPipeline class")
+            return data_ingestion_artifact
         except Exception as e:
             raise MyException(e, sys) from e
-
-    def get_vehicle_input_data_frame(self)-> DataFrame:
-        """
-        This function returns a DataFrame from USvisaData class input
-        """
-        try:
-            
-            vehicle_input_dict = self.get_vehicle_data_as_dict()
-            return DataFrame(vehicle_input_dict)
         
-        except Exception as e:
-            raise MyException(e, sys) from e
-
-
-    def get_vehicle_data_as_dict(self):
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
         """
-        This function returns a dictionary from VehicleData class input
+        This method of TrainPipeline class is responsible for starting data validation component
         """
-        logging.info("Entered get_usvisa_data_as_dict method as VehicleData class")
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
 
         try:
-            input_data = {
-                "Gender": [self.Gender],
-                "Age": [self.Age],
-                "Driving_License": [self.Driving_License],
-                "Region_Code": [self.Region_Code],
-                "Previously_Insured": [self.Previously_Insured],
-                "Annual_Premium": [self.Annual_Premium],
-                "Policy_Sales_Channel": [self.Policy_Sales_Channel],
-                "Vintage": [self.Vintage],
-                "Vehicle_Age_lt_1_Year": [self.Vehicle_Age_lt_1_Year],
-                "Vehicle_Age_gt_2_Years": [self.Vehicle_Age_gt_2_Years],
-                "Vehicle_Damage_Yes": [self.Vehicle_Damage_Yes]
-            }
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
+                                             data_validation_config=self.data_validation_config
+                                             )
 
-            logging.info("Created vehicle data dict")
-            logging.info("Exited get_vehicle_data_as_dict method as VehicleData class")
-            return input_data
+            data_validation_artifact = data_validation.initiate_data_validation()
 
+            logging.info("Performed the data validation operation")
+            logging.info("Exited the start_data_validation method of TrainPipeline class")
+
+            return data_validation_artifact
         except Exception as e:
             raise MyException(e, sys) from e
-
-class VehicleDataClassifier:
-    def __init__(self,prediction_pipeline_config: VehiclePredictorConfig = VehiclePredictorConfig(),) -> None:
+        
+    def start_data_transformation(self, data_ingestion_artifact: DataIngestionArtifact, data_validation_artifact: DataValidationArtifact) -> DataTransformationArtifact:
         """
-        :param prediction_pipeline_config: Configuration for prediction the value
+        This method of TrainPipeline class is responsible for starting data transformation component
         """
         try:
-            self.prediction_pipeline_config = prediction_pipeline_config
+            data_transformation = DataTransformation(data_ingestion_artifact=data_ingestion_artifact,
+                                                     data_transformation_config=self.data_transformation_config,
+                                                     data_validation_artifact=data_validation_artifact)
+            data_transformation_artifact = data_transformation.initiate_data_transformation()
+            return data_transformation_artifact
+        except Exception as e:
+            raise MyException(e, sys)
+        
+    def start_model_trainer(self, data_transformation_artifact: DataTransformationArtifact) -> ModelTrainerArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting model training
+        """
+        try:
+            model_trainer = ModelTrainer(data_transformation_artifact=data_transformation_artifact,
+                                         model_trainer_config=self.model_trainer_config
+                                         )
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            return model_trainer_artifact
+
         except Exception as e:
             raise MyException(e, sys)
 
-    def predict(self, dataframe) -> str:
+    def start_model_evaluation(self, data_ingestion_artifact: DataIngestionArtifact,
+                               model_trainer_artifact: ModelTrainerArtifact) -> ModelEvaluationArtifact:
         """
-        This is the method of VehicleDataClassifier
-        Returns: Prediction in string format
+        This method of TrainPipeline class is responsible for starting modle evaluation
         """
         try:
-            logging.info("Entered predict method of VehicleDataClassifier class")
-            model = Proj1Estimator(
-                bucket_name=self.prediction_pipeline_config.model_bucket_name,
-                model_path=self.prediction_pipeline_config.model_file_path,
-            )
-            result =  model.predict(dataframe)
+            model_evaluation = ModelEvaluation(model_eval_config=self.model_evaluation_config,
+                                               data_ingestion_artifact=data_ingestion_artifact,
+                                               model_trainer_artifact=model_trainer_artifact)
+            model_evaluation_artifact = model_evaluation.initiate_model_evaluation()
+            return model_evaluation_artifact
+        except Exception as e:
+            raise MyException(e, sys)
+
+    def start_model_pusher(self, model_evaluation_artifact: ModelEvaluationArtifact) -> ModelPusherArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting model pushing
+        """
+        try:
+            model_pusher = ModelPusher(model_evaluation_artifact=model_evaluation_artifact,
+                                       model_pusher_config=self.model_pusher_config
+                                       )
+            model_pusher_artifact = model_pusher.initiate_model_pusher()
+            return model_pusher_artifact
+        except Exception as e:
+            raise MyException(e, sys)
+
+    def run_pipeline(self, ) -> None:
+        """
+        This method of TrainPipeline class is responsible for running complete pipeline
+        """
+        try:
+            data_ingestion_artifact = self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+            data_transformation_artifact = self.start_data_transformation(
+                data_ingestion_artifact=data_ingestion_artifact, data_validation_artifact=data_validation_artifact)
+            model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
+            model_evaluation_artifact = self.start_model_evaluation(data_ingestion_artifact=data_ingestion_artifact,
+                                                                    model_trainer_artifact=model_trainer_artifact)
+            if not model_evaluation_artifact.is_model_accepted:
+                logging.info(f"Model not accepted.")
+                return None
+            model_pusher_artifact = self.start_model_pusher(model_evaluation_artifact=model_evaluation_artifact)
             
-            return result
-        
         except Exception as e:
             raise MyException(e, sys)
